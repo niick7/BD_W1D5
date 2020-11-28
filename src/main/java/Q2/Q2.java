29924daaf6c2afcf7895ddffaa5bb5dc8dd300db
Nhan Vo <niick7@gmail.com>
2020-11-27 23:54:03 -0600

    First commit

---
+++ src/main/java/Q2/Q2.java
@@ -0,0 +1,7 @@
+package Q2;
+
+public class Q2 {
+  public static void main(String[] args) {
+    System.out.println("Updating...");
+  }
+}
---
+++ src/main/java/Q3/Q3.java
@@ -0,0 +1,7 @@
+package Q3;
+
+public class Q3 {
+  public static void main(String[] args) {
+    System.out.println("Updating...");
+  }
+}
